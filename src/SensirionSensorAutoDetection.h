d1a346c3db5b736fe347472ca006e7cc1ff18981
Jonas Stolle <jonas.stolle@sensirion.com>
2021-10-21 15:38:38 +0200

    Add missing files to the skeleton
     * Example usage
     * Empty header and source file
     * Changelog

---
+++ src/SensirionSensorAutoDetection.h
@@ -0,0 +1,7 @@
+// include dependent drivers
+#include <Arduino.h>
+#include "SensirionI2CSht4x.h"
+#include "SensirionI2CScd4x.h"
+#include "SensirionI2CSfa3x.h"
+#include "SensirionI2CSvm40.h"
+#include "SensirionI2CSen44.h"
