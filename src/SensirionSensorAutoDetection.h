ab5972bae0704044d38a6e54ba12d77d167decd3
Jonas Stolle <jonas.stolle@sensirion.com>
2021-10-22 16:41:50 +0200

    Fix source formatting with clang-format

--- src/SensirionSensorAutoDetection.h
+++ src/SensirionSensorAutoDetection.h
@@ -1,7 +1,8 @@
 // include dependent drivers
-#include <Arduino.h>
-#include "SensirionI2CSht4x.h"
+
 #include "SensirionI2CScd4x.h"
-#include "SensirionI2CSfa3x.h"
-#include "SensirionI2CSvm40.h"
 #include "SensirionI2CSen44.h"
+#include "SensirionI2CSfa3x.h"
+#include "SensirionI2CSht4x.h"
+#include "SensirionI2CSvm40.h"
+#include <Arduino.h>
