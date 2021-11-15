5c6162adcc8e2be397ac0419a9e6b596cf1e7844
Jonas Stolle <jonas.stolle@sensirion.com>
2021-11-15 13:24:32 +0100

    Instantiate SensorManager in exampleUsage

--- examples/exampleUsage/exampleUsage.ino
+++ examples/exampleUsage/exampleUsage.ino
@@ -1,10 +1,20 @@
 #include "Scd4x.h"
 #include "SensirionSensorAutoDetection.h"
+#include "I2CAutoDetector.h"
+
+
+I2CAutoDetector detector;
+SensorManager sm(detector);
 
 Scd4x scd(Wire);
 
 void setup() {
+    Serial.begin(115200);
+    Serial.println();
+    Wire.begin();
 }
 
 void loop() {
+    Serial.println("loop");
+    delay(1000);
 }
