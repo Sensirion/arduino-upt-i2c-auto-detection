14ea2b95a31ecb7689d09ae93a036a3ebd6d39d8
Jonas Stolle <jonas.stolle@sensirion.com>
2021-11-16 08:37:29 +0100

    Instantiate sensorList and call findSensors in exampleUsage.ino

--- examples/exampleUsage/exampleUsage.ino
+++ examples/exampleUsage/exampleUsage.ino
@@ -3,18 +3,23 @@
 #include "I2CAutoDetector.h"
 
 
-I2CAutoDetector detector(Wire);
-SensorManager sm(detector);
+SensorList sl;
+I2CAutoDetector* pDetector;
+SensorManager* pSm;
 
 Scd4x scd(Wire);
-
+int i;
 void setup() {
     Serial.begin(115200);
     Serial.println();
     Wire.begin();
+    pDetector = new I2CAutoDetector(Wire);
+    pSm = new SensorManager(*pDetector);
+    pDetector->findSensors(sl);
 }
 
 void loop() {
     Serial.println("loop");
     delay(1000);
+    i++;
 }
