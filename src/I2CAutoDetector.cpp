b3e49ab81bfcdc68ad8557596c6a81d49c64eb76
Jonas Stolle <jonas.stolle@sensirion.com>
2021-11-23 12:00:26 +0100

    Make switch/case smaller to prevent duplicate code

--- src/I2CAutoDetector.cpp
+++ src/I2CAutoDetector.cpp
@@ -45,40 +45,43 @@ void I2CAutoDetector::findSensors(SensorList& sensorList) {
         }
         Serial.print(address, HEX);
         Serial.print(": ");
+        ISensor* pSensor = nullptr;
 
         switch (address) {
             case (0x62): {
                 Serial.print("Adding SCD4X .. ");
-                ISensor* pSensor = new Scd4x(_wire);
-                error = pSensor->start();
-                if (error) {
-                    char errorMessage[256];
-                    Serial.print("\nError trying to start() sensor instance: ");
-                    errorToString(error, errorMessage, 256);
-                    Serial.println(errorMessage);
-                    delete pSensor;
-                    break;
-                }
-                error = sensorList.addSensor(
-                    pSensor);  // not in line with sensirion core error
-                               // messages
-                if (error) {
-                    Serial.println("\nError trying to add sensor instance "
-                                   "to sensorList.");
-                    delete pSensor;
-                } else {
-                    nSensors++;
-                    Serial.println("Success!");
-                }
-                break;
+                pSensor = new Scd4x(_wire);
             }
+
                 // further cases
 
-            default:
+            default: {
                 Serial.println("No matching sensor!");
+                continue;
+            }
+        }
+
+        error = pSensor->start();
+        if (error) {
+            char errorMessage[256];
+            Serial.print("\nError trying to start() sensor instance: ");
+            errorToString(error, errorMessage, 256);
+            Serial.println(errorMessage);
+            delete pSensor;
+            continue;
+        }
+        error = sensorList.addSensor(pSensor);  // not in line with sensirion
+                                                // core error messages
+        if (error) {
+            Serial.println("\nError trying to add sensor instance "
+                           "to sensorList.");
+            delete pSensor;
+        } else {
+            nSensors++;
+            Serial.println("Success!");
         }
     }
-}
-if (nSensors == 0) {
-    Serial.println("No sensors detected!");
+    if (nSensors == 0) {
+        Serial.println("No sensors detected!");
+    }
 }
