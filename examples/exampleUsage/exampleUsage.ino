66db0a25e2fe96bed6f11d7ebef868dac79d954e
Jonas Stolle <jonas.stolle@sensirion.com>
2021-12-23 15:13:00 +0100

    Include all features in exampleUsage

--- examples/exampleUsage/exampleUsage.ino
+++ examples/exampleUsage/exampleUsage.ino
@@ -6,10 +6,12 @@
 void printData(const Data& data) {
     for (size_t i = 0; i < data.getLength(); ++i) {
         const DataPoint& dp = data.dataPoints[i];
+        // Get SensorId string using SensorId enum as index
         Serial.print(sensorIdStr[static_cast<int>(dp.id)]);
         Serial.print(": ");
         Serial.print(dp.value);
         Serial.print(" ");
+        // Get Unit string using Unit enum as index
         Serial.print(unitStr[static_cast<int>(dp.unit)]);
         Serial.print(" @");
         Serial.println(dp.timeStamp);
@@ -17,8 +19,8 @@ void printData(const Data& data) {
     Serial.println();
 }
 
-I2CAutoDetector i2CAutoDetector(Wire);
-SensorManager sensorManager(i2CAutoDetector);
+I2CAutoDetector i2CAutoDetector(Wire); // Instantiate I2CAutoDetector
+SensorManager sensorManager(i2CAutoDetector); // Instan
 SensirionI2CScd4x* pScd4xDriver = nullptr;
 
 void setup() {
@@ -26,12 +28,37 @@ void setup() {
     Serial.println();
     Wire.begin();
     sensorManager.init();
-    delay(5000);
+
+    // Retrieving the sensor driver
+    AutoDetectorError error = sensorManager.getSensorDriver<SensirionI2CScd4x>(
+        pScd4xDriver, SensorId::SCD4X);
+    if (error) {
+        Serial.println("Scd4x driver retrieval failed!");
+    } else {
+        Serial.println("SCD4X driver retrieval succeeded!\n");
+        delay(5000);  // Wait for SCD4X to finish measuring
+        uint16_t co2;
+        float temperature;
+        float humidity;
+        pScd4xDriver->readMeasurement(co2, temperature, humidity);
+        Serial.println("Data retrieved directly from sensor driver:");
+        Serial.print("co2: ");
+        Serial.println(co2);
+        Serial.print("temperature: ");
+        Serial.println(temperature);
+        Serial.print("humidity: ");
+        Serial.println(humidity);
+        Serial.println();
+    }
+    delay(5000);  // Wait for SCD4X to finish measuring
+
+    // Setting the SCD4X measurement interval
+    sensorManager.setInterval(10000, SensorId::SCD4X);
 }
 
 void loop() {
     delay(1000);
-    // Get data via sensorManager
+    // Get data via SensorManager
     AutoDetectorError error = sensorManager.updateData();
     if (error) {
         return;
