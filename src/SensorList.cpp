2a527bcfe22b5250995ce4d13de289161d68df16
Quentin <quentin.fisch@sensirion.com>
2024-10-28 10:48:27 +0100

    replaced prints by ESP logs in library code

--- src/SensorList.cpp
+++ src/SensorList.cpp
@@ -1,5 +1,7 @@
 #include "SensorList.h"
 
+static const char* TAG = "SensorList";
+
 size_t SensorList::_hashSensorType(SensorType sensorType) const {
     SensorHash sensorHash = SensorHash::_UNDEFINED;
     switch (sensorType) {
@@ -41,8 +43,7 @@ size_t SensorList::_hashSensorType(SensorType sensorType) const {
     }
 
     if (sensorHash == SensorHash::_UNDEFINED) {
-        Serial.printf("Error: invalid sensor type %s encountered. Aborting!\n",
-                      sensorLabel(sensorType));
+        ESP_LOGE(TAG, "Error: invalid sensor type %s encountered. Aborting!", sensorLabel(sensorType));
         assert(0);
     }
 
