dfddcbb531779f62ed896e0ada318fe339ac2a4b
Rolf Laich <rolf.laich@sensirion.com>
2025-05-15 17:55:10 +0200

    Refactor autodetection
    Add a sensor mapping object and simplify the detection algorithm.

--- src/I2CAutoDetector.h
+++ src/I2CAutoDetector.h
@@ -2,24 +2,77 @@
 #define I2C_AUTO_DETECTOR_H
 
 #include "IAutoDetector.h"
+#include "I2cSensorMapping.h"
 #include <Wire.h>
+#include <array>
 
+
+/// Class to search registered sensors on the i2c bus
+///
+/// @tparam ...SensorMappingT Address to sensor type mapping
+template<typename...SensorMappingT>
 class I2CAutoDetector : public IAutoDetector {
+
   public:
-    explicit I2CAutoDetector(TwoWire& wire) : _wire(wire){};
+    /// defines the number of configured sensors
+    static constexpr size_t CONFIGURED_SENSORS = sizeof...(SensorMappingT);
+
+
+    explicit I2CAutoDetector(TwoWire& wire):
+      _wire(wire),
+      mDetectionTable{createMappingInstance<SensorMappingT>(wire)...}{};
+
+    virtual ~I2CAutoDetector() {
+      for( auto entry : mDetectionTable){
+        delete entry;
+      }
+    }
+
+    /** 
+     * Get the number of configured sensors
+     * 
+     * @return The number of sensors that can be configured
+    */
+    virtual size_t configuredSensorsCount() const override{
+      return CONFIGURED_SENSORS;
+    }
 
     /**
      * @brief scan i2c bus for available sensors
      *
      * @param sensorList SensorList to which add the found sensors
      */
-    void findSensors(SensorList& sensorList) override;
+    virtual void findSensors(SensorList& sensorList) override {
+      for (auto tableEntry:mDetectionTable){      
+        _wire.beginTransmission(tableEntry->getI2cAddress());
+        const byte error = _wire.endTransmission();
+        if (error){
+            continue;
+        }
+        sensorList.addSensor(&tableEntry->getSensor());
+      }
+    }
+  
 
   private:
+    using DetectableSensorsT = std::array<ISensorToAddressMapping*, 
+          sizeof...(SensorMappingT)>;
+
+    DetectableSensorsT mDetectionTable;
+
     TwoWire& _wire;
-    SensorType probeAddress(const byte& address) const;
-    ISensor* createSensorFromSensorType(SensorType sensorType) const;
-    static SensorType getSensorTypeFromAddress(byte address);
+
+    /// Helper funciton to implement the initialisation of the detection table
+    ///
+    /// Required for fold expression
+    /// @tparam T The type of the SensorMapping to be constructed.
+    /// @param wire Parameter to the constructor of T
+    /// @return A pointer of type ISensorToAddressMapping
+    template<typename T>
+    ISensorToAddressMapping* createMappingInstance(TwoWire& wire){
+      return new T(wire);
+    }
 };
+
 
 #endif /* I2C_AUTO_DETECTOR_H */
---
+++ src/I2cSensorMapping.h
@@ -0,0 +1,53 @@
+#ifndef I2C_SENSOR_MAPPING_H
+#define I2C_SENSOR_MAPPING_H
+
+#include "IAutoDetector.h"
+#include <Wire.h>
+#include <array>
+#include <vector>
+
+
+/// This is the interface of an entry in the registration table
+/// that allows to map sensors to i2c addresses.
+/// We define this interface in order to allow type erasure.
+struct ISensorToAddressMapping{
+  using SensorRef = ISensor&;
+
+  virtual uint8_t getI2cAddress() const = 0;
+  virtual SensorRef getSensor() = 0;
+  virtual ~ISensorToAddressMapping(){}
+};
+
+/// Defines an entry int the sensor registation table.
+///
+/// @tparam address The i2c address
+/// @tparam SensorT The sensor classe that lists on the specified i2c address
+template<uint8_t address, typename SensorT>
+struct SensorToAddressMapping: ISensorToAddressMapping{
+
+  static constexpr uint8_t I2C_ADDRESS = address;
+
+  explicit SensorToAddressMapping(TwoWire& wire):
+    mSensor(wire, I2C_ADDRESS){}
+
+  /// No copy, assign, move
+  SensorToAddressMapping(const SensorToAddressMapping& other) = delete;
+  SensorToAddressMapping(const SensorToAddressMapping&& other) = delete;
+  SensorToAddressMapping& operator=(const SensorToAddressMapping& other) = delete;
+
+  
+  virtual uint8_t getI2cAddress()  const override{
+    return I2C_ADDRESS;
+  }
+
+  virtual SensorRef getSensor() override{
+    return mSensor;
+  }
+
+  private:
+
+    SensorT mSensor;
+};
+
+
+#endif // I2C_SENSOR_MAPPING_H
