575d9cc7681f80d63c3a650b84914219b74f6aa6
qfisch <quentin.fisch@sensirion.com>
2026-01-05 13:44:05 +0100

    Added STCC4 probe function

--- src/SensorWrappers/Stcc4.h
+++ src/SensorWrappers/Stcc4.h
@@ -19,6 +19,7 @@ class Stcc4 : public ISensor {
     size_t getNumberOfDataPoints() const override;
     uint8_t getI2CAddress() const override;
     unsigned long getMinimumMeasurementIntervalMs() const override;
+    bool probe() override;
     void* getDriver() override;
 
   private:
